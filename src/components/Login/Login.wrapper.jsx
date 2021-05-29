b5d94705f7beaca53abfdc9e93feb1fdf7cb96d7
Harshal Deshpande <harshal25012001@gmail.com>
2021-05-29 10:29:09 +0530

    login done without routing

--- src/components/Login/Login.wrapper.jsx
+++ src/components/Login/Login.wrapper.jsx
@@ -1,92 +1,162 @@
-import React, { PureComponent } from 'react';
-import { Grid, Paper, TextField, Button, Typography, Link } from '@material-ui/core'
+import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import './Login.css'
 
-export default class LoginWrapper extends PureComponent {
-  
-  render() {
-    const paperStyle={padding: 30, height:'70vh', width:'90vw', maxWidth:'400px'}
-    const btnstyle={margin:'8px 0'}
-    const {loading, errors, data, actions} = this.props
-    const email = 'harshal@gmail.com'
-    const password = 'password'
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', 
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-    return (
-      <div>
-        <Paper elevation={5} style={paperStyle}>
-        <Grid>
-                <Grid align='center'>
-                    <h2 className='brand-header'>Login</h2>
-                    <h5>{errors}</h5>
-                    <h5>{data.email}</h5>
-                </Grid>
-                <Typography > Don&apos;t have an account ?
-                     <Link href="/login" >
-                        Sign Up 
-                </Link>
-                </Typography>
-                <TextField className='input-field' label='Username' fullWidth required/>
-                <TextField className='input-field' label='Password' type='password' fullWidth required/>
-                <FormControlLabel className='remember-me'
-                    control={
-                    <Checkbox
-                        name="checkedB"
-                        color="primary"
-                    />
-                    }
-                    label="Remember me"
-                 />
-                { loading && <CircularProgress /> }
-                  <Button 
-                    type='submit' 
-                    color='primary' 
-                    variant="contained" 
-                    style={btnstyle} 
-                    fullWidth
-                    disabled={loading}
-                    onClick={() => actions.login({ email, password})}
-                  >
-                    Sign in
-                  </Button>
-                {/* <Button 
-                    type='button' 
-                    color='primary' 
-                    variant="contained" 
-                    style={btnstyle} 
-                    fullWidth
-                    onClick={() => actions.logout()}
-                >
-                  Logout
-                </Button> */}
-                <div className='forgot-password'>
-                  <div>
-                    <Typography >
-                      <Link href="/login" >
-                        Forgot password ?
-                      </Link>
-                    </Typography>
-                  </div>
-                </div>
-                
-        </Grid>
-        </Paper>
-        {/* <h4>Login</h4>
-        <h5>{errors}</h5>
-        <h5>{data.email}</h5>
-        {loading?
-          <div>Loading...</div>
-          :
-          <button onClick={() => actions.login({ email, password})} type='submit'> 
-            Submit 
-          </button>
-        } */}
-        {/* <button onClick={() => actions.logout()} type='button'> 
-            Logout
-        </button> */}
-      </div>
-    );
+export default function LoginWrapper(props) {
+  const classes = useStyles();
+  const {loading, errors, data, actions} = props
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(email) {
+      setEmailError('');
+      if(password) {
+        if(password.length >= 6) {
+          setPasswordError('');
+          actions.login({email, password});
+        }
+        else {
+          return setPasswordError('Minimun 6 characters');
+        }
+      }
+      else {
+        return setPasswordError('Password required');
+      }
+    }
+    else {
+      return setEmailError('Email required');
+    }
+    return true;
   }
+
+  return (
+    <Container component="main" maxWidth="xs" className="login-container">
+      <CssBaseline />
+      <div className={classes.paper}>
+
+        <Typography component="h1" variant="h5">
+          <span style={{fontWeight: 'bolder', fontSize: '1.3em'}}>Login</span>
+        </Typography>
+
+        {data.email}
+        {error}
+        {errors}
+
+        <Grid container style={{marginTop: '2.5em'}}>
+            <Grid item xs>
+              Dont&apos; have an account?  
+              <Link href="/" variant="body2">
+              &nbsp;Sign Up
+              </Link>
+            </Grid>
+        </Grid>
+
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            error = {emailError}
+            helperText = {emailError}
+            value = {email}
+            onChange = {(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            error = {passwordError}
+            helperText = {passwordError}
+            value = {password}
+            onChange = {(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          {loading ? 
+          <Grid container align="center">
+            <Grid item xs>
+              <CircularProgress/>
+            </Grid>
+          </Grid>
+          :
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={loading}
+          >
+            Sign In
+          </Button>
+          }
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={() => actions.logout()}
+          >
+            Logout
+          </Button>
+          <Grid container style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <Grid item>
+              <Link href="/" variant="body2">
+                Forgot Password?
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 }
