145d5ae95a4b7afe5420bbbf7cf9d03d79af37b3
Harshal Deshpande <harshal25012001@gmail.com>
2021-05-31 05:29:30 +0530

    refactoring

--- src/components/Name/Name.test.js
+++ src/components/Name/Name.test.js
@@ -4,7 +4,7 @@ import NameContainer from './Name.Container';
 describe('login Comp', () => {
   test('verify name', () => {
     const wrapper = shallow(<NameContainer />);
-    const name = 'Gaurav';
+    // const name = 'Gaurav';
     // console.log('props',wrapper.props());
     // console.log('children',wrapper.props().children[0].props);
     // expect(wrapper.props().children).toBe(name);
