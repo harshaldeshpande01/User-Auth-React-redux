1ba28d0ece79e3cbbd509820f368dd8b1164f77c
Harshal Deshpande <harshal25012001@gmail.com>
2021-06-01 05:35:06 +0530

    utils.global.js logoc updated

--- src/utils/global.js
+++ src/utils/global.js
@@ -3,8 +3,10 @@ import {store} from '../configurations/redux/store';
 const isAuthenticated = () => {
     const state = store.getState();
     const {data} = state.auth;
-    if(data.email) {
-        return true;
+    if(data !== undefined) {
+        if(data.email) {
+            return true;
+        }
     }
     return false;
 };
